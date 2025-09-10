108837d224df3cd76939965a96a21f31085b42b9
SANDEEP KURUBA <sandeepkuruba214@gmail.com>
2025-09-10 20:00:18 +0530

    Add required assets for build

---
+++ client/src/assets/assets.js
@@ -0,0 +1,52 @@
+
+import facebook_icon from './facebook_icon.svg'
+import googleplus_icon from './googleplus_icon.svg'
+import logo from "./logo.svg"
+import add_icon from './add_icon.svg'
+import list_icon from './list_icon.svg'
+import email_icon from './email_icon.png'
+import upload_area from './upload_area.svg'
+import user_icon from './user_icon.svg'
+import bin_icon from './bin_icon.svg'
+import comment_icon from './comment_icon.svg'
+import tick_icon from './tick_icon.svg'
+import star_icon from './star_icon.svg'
+import cross_icon from './cross_icon.svg'
+import home_icon from './home_icon.svg'
+import gradientBackground from './gradientBackground.png'
+import dashboard_icon_1 from './dashboard_icon_1.svg'
+import dashboard_icon_2 from './dashboard_icon_2.svg'
+import dashboard_icon_3 from './dashboard_icon_3.svg'
+import dashboard_icon_4 from './dashboard_icon_4.svg'
+import profile_icon from "./profile_icon.svg"
+import delete_icon from "./delete_icon.svg"
+import allusers from "./allusers.svg"
+
+
+export const assets = {
+    facebook_icon,
+    googleplus_icon,
+    add_icon,
+    email_icon,
+    upload_area,
+    user_icon,
+    bin_icon,
+    comment_icon,
+    tick_icon,
+    star_icon,
+    home_icon,
+    gradientBackground,
+    list_icon,
+    cross_icon,
+    dashboard_icon_1,
+    dashboard_icon_2,
+    dashboard_icon_3,
+    dashboard_icon_4,
+    profile_icon,
+    delete_icon,
+    allusers,
+    logo
+    
+    
+}
+export const blogCategories = ['All', 'Technology', 'Lifestyle', 'Finance', "Education",'News & Current Affairs',"Travelling","Health"]
