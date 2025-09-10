c5b9eebba3fa0dd3422414372c0939c5ec70f4fe
SANDEEP KURUBA <sandeepkuruba214@gmail.com>
2025-09-10 22:27:00 +0530

    Updated frontend axios baseURL to Render backend

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true, // Always send cookies
+  baseURL: import.meta.env.VITE_BASE_URL + "/api",
+  withCredentials: true,
 });
 
 export default API;
